67c5b784d88187637284b32fa6d6c7e56541bb4d
codediodeio <hello@codediode.io>
2017-08-29 17:04:50 -0700

    spread update

--- src/app/reducers/post.reducer.ts
+++ src/app/reducers/post.reducer.ts
@@ -5,9 +5,9 @@ export type Action = PostActions.All;
 
 
 /// Converts state objects to new state with ... spread operator
-const newState = (...stateObjects) => {
-  return Object.assign({}, ...stateObjects);
-};
+// const newState = (...stateObjects) => {
+//   return Object.assign({}, ...stateObjects);
+// };
 
 
 /// Reducer function
@@ -16,19 +16,19 @@ export function postReducer(state: Post, action: Action) {
   switch (action.type) {
 
     case PostActions.GET_POST:
-      return newState(state, { loading: true });
+      return { ...state, loading: true };
 
     case PostActions.GET_POST_SUCCESS:
-      return newState(state, action.payload, { loading: false } );
+      return { ...state, ...action.payload, loading: false };
 
     case PostActions.VOTE_UPDATE:
-      return newState(state, action.payload, { loading: true } );
+      return { ...state, ...action.payload, loading: true };
 
     case PostActions.VOTE_SUCCESS:
-      return newState(state, { loading: false });
+      return { ...state, loading: false };
 
     case PostActions.VOTE_FAIL:
-      return newState(state, action.payload, { loading: false });
+      return { ...state, ...action.payload, loading: false };
 
     default:
       return state;
